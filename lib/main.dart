80d3ea76f7a79a49b9a41db5c50ec4eb96ad8d9a
Ahmed Abdelghany  Ahmed <anofal786@gmail.com>
2025-01-02 15:15:52 +0200

    finish the app

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _MyAppState extends State<MyApp> {
     isLookingRight = false;
   }
 
-  void addSpecifcAnimationAction(
+  void addSpecificAnimationAction(
       RiveAnimationController<dynamic> neededAnimationAction) {
     removeAllControllers();
     myArtboard?.artboard.addController(neededAnimationAction);
@@ -72,8 +72,7 @@ class _MyAppState extends State<MyApp> {
     controllerFail = SimpleAnimation(AnimationEnum.fail.name);
     controllerHandsUp = SimpleAnimation(AnimationEnum.Hands_up.name);
     controllerHandsDown = SimpleAnimation(AnimationEnum.hands_down.name);
-    controllerLookDownRight =
-        SimpleAnimation(AnimationEnum.Look_down_right.name);
+    controllerLookDownRight = SimpleAnimation(AnimationEnum.Look_down_right.name);
     controllerLookDownLeft = SimpleAnimation(AnimationEnum.Look_down_left.name);
     controllerLookIdle = SimpleAnimation(AnimationEnum.look_idle.name);
 
@@ -90,9 +89,9 @@ class _MyAppState extends State<MyApp> {
    void checkForPasswordFocusNodeToChangeAnimationState() {
     passwordFocusNode.addListener(() {
       if (passwordFocusNode.hasFocus) {
-        addSpecifcAnimationAction(controllerHandsUp);
+        addSpecificAnimationAction(controllerHandsUp);
       } else if (!passwordFocusNode.hasFocus) {
-        addSpecifcAnimationAction(controllerHandsDown);
+        addSpecificAnimationAction(controllerHandsDown);
       }
     });
   }
@@ -100,9 +99,9 @@ class _MyAppState extends State<MyApp> {
    void validateEmailAndPassword() {
     Future.delayed(const Duration(seconds: 1), () {
       if (formKey.currentState!.validate()) {
-        addSpecifcAnimationAction(controllerSuccess);
+        addSpecificAnimationAction(controllerSuccess);
       } else {
-        addSpecifcAnimationAction(controllerFail);
+        addSpecificAnimationAction(controllerFail);
       }
     });
   }
@@ -118,90 +117,88 @@ class _MyAppState extends State<MyApp> {
           centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 3,
-                child: myArtboard == null
-                    ? const SizedBox.shrink()
-                    : Rive(
-                        artboard: myArtboard!,
-                      ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 50,
-              ),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: myArtboard == null
+                      ? const SizedBox.shrink()
+                      : Rive(
+                          artboard: myArtboard!,
                         ),
-                      ),
-                      validator: (value) =>
-                          value != testEmail ? "Wrong email" : null,
-                      onChanged: (value) {
-                        if (value.isNotEmpty &&
-                            value.length < 16 &&
-                            !isLookingLeft) {
-                          addSpecifcAnimationAction(controllerLookDownLeft);
-                        } else if (value.isNotEmpty &&
-                            value.length > 16 &&
-                            !isLookingRight) {
-                          addSpecifcAnimationAction(controllerLookDownRight);
-                        }
-                      },
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height / 25,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                      ),
-                      focusNode: passwordFocusNode,
-                      onTap: checkForPasswordFocusNodeToChangeAnimationState,
-                      validator: (value) =>
-                          value != testPassword ? "Wrong password" : null,
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height / 18,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width / 8,
-                      ),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          shape: const StadiumBorder(),
-                          backgroundColor: Colors.blue,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                        ),
-                        onPressed: () {
-                          passwordFocusNode.unfocus();
-                          validateEmailAndPassword();
-                        },
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
-              )
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 18,
+                ),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        validator: (value) =>
+                            value != testEmail ? "Wrong email" : null,
+                        onChanged: (value) {
+                          if (value.isNotEmpty && value.length < 25 && !isLookingLeft) {
+                            addSpecificAnimationAction(controllerLookDownLeft);
+                          } else if (value.isNotEmpty && value.length > 25 && !isLookingRight) {
+                            addSpecificAnimationAction(controllerLookDownRight);
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 25,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        focusNode: passwordFocusNode,
+                        onTap: checkForPasswordFocusNodeToChangeAnimationState,
+                        validator: (value) =>
+                            value != testPassword ? "Wrong password" : null,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 18,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width / 8,
+                        ),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            shape: const StadiumBorder(),
+                            backgroundColor: Colors.blue,
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                          ),
+                          onPressed: () {
+                            passwordFocusNode.unfocus();
+                            validateEmailAndPassword();
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
